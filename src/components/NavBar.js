dcfd9ad44bb1ea3ec8db458e59789195c4496ad7
Jocyline Magero <jmagero2017@gmail.com>
2021-08-30 16:15:25 -0500

    Add NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Book',
+    },
+
+    {
+        id: 2,
+        path: '/categories',
+        text: 'Categories',
+      },
+  ];
+  return (
+    <>
+      <nav>
+        <h1>
+          BookStore JM
+        </h1>
+        <ul className="nav-links">
+          {links.map((link) => (
+            <li key={link.id}>
+                <NavLink to={link.path}>{link.text}</NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
