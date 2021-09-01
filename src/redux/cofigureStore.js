a45aedfe7ca9fbfdeb01c615e2a62ca601f93ac0
Jocyline Magero <jmagero2017@gmail.com>
2021-09-01 12:11:35 -0500

    Fix lint errors

--- src/redux/cofigureStore.js
+++ src/redux/cofigureStore.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import booksReducer from './books/books';
