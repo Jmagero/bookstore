2d812b6aacabe1196ac2586127cbf6f92c54732d
Jocyline Magero <jmagero2017@gmail.com>
2021-09-03 10:44:50 -0500

    render display book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,14 +1,25 @@
+/* eslint-disable */
 import AddBook from './AddBook';
 import DisplayBook from './DisplayBooks';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchBooks } from '../redux/books/books';
 
-const Book = () => (
-  <div>
+const Book = () => {
+  const store = useSelector((state) => state);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, []);
+  const bookList = store.booksReducer;
+
+  return (
+    <div>
     <h3>BOOKS</h3>
-    <ul>
-      <DisplayBook />
-    </ul>
-    <AddBook />
-  </div>
-);
+      <DisplayBook bookList={bookList} />
+      <AddBook />
+    </div>
+  )
+}
 
 export default Book;
