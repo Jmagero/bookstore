0bb64dfb9fbf5844097692ea2ce989cab722f036
Jocyline Magero <jmagero2017@gmail.com>
2021-09-03 10:40:34 -0500

    Generate new app id

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,7 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const API = 'OCEGl7wWVt4ipESKaF4S'
+const URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi'
 
 const initialState = [];
 
@@ -12,6 +14,11 @@ export const removeBook = (id) => ({
   id,
 });
 
+export const getID = () => {
+    axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/')
+    .then( response => response.data)
+    .catch( err => console.log(err))
+}
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
