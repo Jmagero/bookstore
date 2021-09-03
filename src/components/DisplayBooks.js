5af73b79a93f9da6e388709afdda40f03c15e1d5
Jocyline Magero <jmagero2017@gmail.com>
2021-09-03 18:37:39 -0500

    Add style to display book container

--- src/components/DisplayBooks.js
+++ src/components/DisplayBooks.js
@@ -1,7 +1,9 @@
+/* eslint-disable */
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import React from 'react';
 import { fetchRemoveBook } from '../redux/books/bookActions';
+import styles from './DisplayBooks.module.css';
 
 const DisplayBook = (props) => {
   const dispatch = useDispatch();
@@ -12,23 +14,34 @@ const DisplayBook = (props) => {
   };
 
   return (
-    <div>
-      <ul>
+      <ul className={styles.booksContainer}>
         {bookList.map((book) => (
-          <li key={book.item_id}>
-            <p>
-              Title:
-              { book.title }
-            </p>
-            <p>
-              Author:
-              { book.category }
-            </p>
-            <button type="button" onClick={() => { removeBookStore(book.item_id); }}>Remove Book</button>
+          <li key={book.item_id}
+          className={styles.liCard}>
+            <div className={styles.bookCard}>
+              <p className={styles.title}>
+                { book.title }
+              </p>
+              <p className={styles.category}>
+                { book.category }
+              </p>
+              <button type="button" className={styles.buttons} onClick={() => { removeBookStore(book.item_id); }}>Remove Book</button>
+            </div>
+            <div className={styles.completedDiv}>
+              <div className={styles.circle} />
+              <div className={styles.completedCard}>
+                <p className={`${styles.percentage} ${styles.font1Reg}`}>0%</p>
+                <p className={`${styles.completed} ${styles.font1Reg}`}>Completed</p>
+              </div>
+            </div>
+            <div className={styles.updateDiv}>
+              <h3 className={`${styles.current} ${styles.font2Light}`}>Current Chapter</h3>
+              <p className={`${styles.chapter} ${styles.font2Light}`}>Introduction</p>
+              <button type="button" className={styles.update}>UPDATE PROGRESS</button>
+            </div>
           </li>
         ))}
       </ul>
-    </div>
   );
 };
 
