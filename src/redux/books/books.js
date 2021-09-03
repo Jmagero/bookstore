1a77f56f630c3111e67204753d2072ff6f5b145d
Jocyline Magero <jmagero2017@gmail.com>
2021-09-03 10:41:24 -0500

    Add action types for API requests

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,7 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const FETCH_BOOKS = 'bookStore/books/FETCH_BOOKS';
+const REQUEST_BOOKS_FAILURE = 'bookStore/books/REQUEST_BOOKS_FAILURE';
 const API = 'OCEGl7wWVt4ipESKaF4S'
 const URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi'
 
@@ -14,6 +16,19 @@ export const removeBook = (id) => ({
   id,
 });
 
+export const getBooks = (payload) => ({
+  type: FETCH_BOOKS,
+  payload,
+})
+
+export const fetchBooksRequest = () => ({
+  type: REQUEST_BOOKS,
+})
+
+export const fetchFailure = (err) => ({
+  type: REQUEST_BOOKS_FAILURE,
+  err
+})
 export const getID = () => {
     axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/')
     .then( response => response.data)
