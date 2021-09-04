dc70edc3fee1d337c07f57764706579a68acadc1
Jocyline Magero <jmagero2017@gmail.com>
2021-09-04 09:29:40 -0500

    Style Nav to original design

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { BsFillPersonFill } from 'react-icons/bs';
 import styles from './NavBar.module.css';
 
 const NavBar = () => {
@@ -19,16 +20,24 @@ const NavBar = () => {
   return (
     <>
       <nav className={styles.NavBar}>
-        <h1 className={styles.BookstoreTitle}>
-          BookStore JM
-        </h1>
-        <ul className={styles.navLinks}>
-          {links.map((link) => (
-            <li key={link.id}>
-              <NavLink className={styles.BOOKS} to={link.path}>{link.text}</NavLink>
-            </li>
-          ))}
-        </ul>
+        <div className={styles.div}>
+          <div className={styles.insideDiv1}>
+            <h1 className={`${styles.BookstoreTitle} ${styles.font1Bold} ${styles.blue}`}>
+              BookStore JM
+            </h1>
+            <ul className={styles.navLinks}>
+              {links.map((link) => (
+                <li key={link.id}>
+                  <NavLink className={`${styles.BOOKS} ${styles.font1Bold}`} to={link.path}>{link.text}</NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className={styles.insideDiv2}>
+            <BsFillPersonFill className={`${styles.icon} ${styles.blue}`} />
+          </div>
+        </div>
+
       </nav>
     </>
   );
